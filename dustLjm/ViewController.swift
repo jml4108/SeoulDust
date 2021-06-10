707c5160687bf0e42f294ed1866c8f447312a7c2
LeeJeongMin <wjdals4108@icloud.com>
2021-06-11 01:19:06 +0900

    Initial Commit

---
+++ dustLjm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dustLjm
+//
+//  Created by 이정민 on 2021/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
