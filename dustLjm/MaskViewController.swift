dda7cd5f082d9574511ddd5dfeecb0d61fd3e36b
ljm4108 <wjdals4108@icloud.com>
2021-08-30 15:34:55 +0900

    test commit

---
+++ dustLjm/MaskViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MaskViewController.swift
+//  dustLjm
+//
+//  Created by 이정민 on 2021/06/12.
+//
+
+import UIKit
+import WebKit
+
+class MaskViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let urlKorString = "https://msearch.shopping.naver.com/search/all?query=마스크&frm=NVSHSRC&cat_id=&pb=true&mall="
+        guard let urlString = urlKorString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+        // Do any additional setup after loading the view.
+    }
+
+}
